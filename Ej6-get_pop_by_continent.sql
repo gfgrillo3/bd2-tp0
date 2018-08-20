51211e269eb9e6d072d41beb89dd091ac4cbc108
andiblas <andy_blas@hotmail.com>
2018-08-20 17:26:57 -0300

    get_pop_by_continent

--- Ej6-get_pop_by_continent.sql
+++ Ej6-get_pop_by_continent.sql
@@ -1,6 +1,12 @@
 --funcion poblacion de continente especifico
 create or replace function get_pop_by_continent(continente integer) returns bigint
 as
+$$
 
 select sum(poblacion_estimada) as total from pais
 where idContinente = continente
+
+$$
+Language SQL
+
+-- select get_pop_by_continent(1)
