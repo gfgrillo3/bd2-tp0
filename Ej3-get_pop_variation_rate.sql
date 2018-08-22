fa7d51395cbc6e14e6a8a04836b432e20fe56e8d
Andres Blas Pujadas <apujadas@hasar.com>
2018-08-22 17:51:27 -0300

    ejercicio 3

--- Ej3-get_pop_variation_rate.sql
+++ Ej3-get_pop_variation_rate.sql
@@ -2,6 +2,20 @@ create
 or replace
 function get_pop_variation_rate( idpais integer ) returns bigint as $$
 
+SELECT 
+	*,
+    c1.poblacion - c2.poblacion as diferencia,
+    (c1.poblacion / c1.anio) / (c2.poblacion / c2.anio) as tasa
+FROM
+    censos c1,
+    censos c2
+WHERE
+	c1.idPais = 11
+    AND c2.idPais = 11
+    AND c1.anio > c2.anio
+ORDER BY
+	diferencia
+
 select
 	sum( poblacion )
 from
